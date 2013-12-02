9e9b28d63517c6a8d5349862bc402732e8982c24
Styx <styx@hidev.it>
2013-12-02 05:40:07 +0200

    Init

---
+++ README.md
@@ -0,0 +1,28 @@
+IamFeelingLucky
+===
+
+**IamFeelingLucky** is the Chrome extension for [http://directory.io](http://directory.io)
+
+Installation
+---
+1. Open `chrome://extensions/` page
+2. Click *Load unpacked extension...* button.
+3. Specify pulled `src` folder
+4. Enjoy
+
+Features
+---
+1. **Random Page** link
+2. Improved display of **current page/total pages** numbers
+3. Automatically fetching **Balance/Transactions** for wallets ([https://blockchain.info](https://blockchain.info))
+
+Usage
+---
+1. Open [http://directory.io](http://directory.io) page
+2. Click **Random Page** link
+3. ...
+4. PROFIT!
+
+License
+---
+**IamFeelingLucky** is available under the [WTFPL](http://sam.zoy.org/wtfpl/ "WTFPL") license. See the LICENSE file for more info.
---
+++ src/helper.js
@@ -0,0 +1,74 @@
+(function(){
+	var i,
+		h1 = document.querySelector('h1'),
+		h2 = document.querySelector('h2'),
+		list = document.querySelectorAll('.keys a[href*="blockchain.info"]'),
+		url = 'http://blockchain.info/multiaddr?limit=0&cors=true&active=';
+
+	if(!h1 || !h2 || !list) {
+		return;
+	}
+	// format current page/total pages
+	h2.innerHTML = h2.innerHTML.replace('Page ','').replace('out of ','\n');
+
+	// generate random page number
+	var maxPage = h2.innerHTML.split('\n')[1];
+	var digits = Math.ceil(Math.random()*maxPage.length);
+	do {
+		var valid = true, page = '';
+		for(i=0;i<digits;i++) {
+			page += Math[i?'floor':'ceil'](Math.random()*(i?10:9));
+		}
+		if(digits==maxPage.length) {
+			for(i=0;i<digits;i++) {
+				var rc = page[i]|0, mc = maxPage[i]|0;
+				if(rc!=mc) {
+					valid = rc<mc;
+					break;
+				}
+			}
+		}
+	} while(!valid);
+	h1.innerHTML += ' <a href="/'+page+'">[Random Page]</a>';
+
+	// check wallets
+	if(!list.length) {
+		return;
+	}
+	for(i=list.length;i--;) {
+		url += list.item(i).text + '%7C';
+	}
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET',url,true);
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState != 4) {
+			return;
+		}
+		var res = false;
+		try {
+			res = JSON.parse(xhr.responseText);
+		} catch(e) {
+			// do nothing
+		}
+		if(res===false || !res.addresses) {
+			return;
+		}
+		var ao = res.addresses, al = {};
+		for(i=ao.length;i--;) {
+			al[ao[i].address] = [ ao[i].final_balance|0, ao[i].n_tx|0 ];
+		}
+		var pre = document.querySelector('.keys');
+		var lines = pre.innerHTML.split('\n'), c = list.length;
+		for(i=lines.length;i--;) {
+			var line = lines[i];
+			if(!line.length || line.charAt(0)==='<') {
+				continue;
+			}
+			var adr = list.item(--c).text, d = al[adr], tf = d[0], tx = d[1], tag = tf>0 ? 'b' : 'i';
+			lines[i] += '\t\t<'+tag+'>'+(tf>0 ? (tf/100000000).toFixed(8).replace(/\.?0+$/g,'') : 0)+'\t\t['+tx+']</'+tag+'>';
+		}
+		pre.innerHTML = lines.join('\n');
+		console.debug('IamFeelingLucky update complete');
+	};
+	xhr.send();
+})();
